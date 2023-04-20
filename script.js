6d19a566f41bd64a639289dfef7f3b83f0150fcd
lavoor <stanislawsynall@gmail.com>
2023-04-20 22:28:43 +0200

    Update script.js

--- script.js
+++ script.js
@@ -21,6 +21,13 @@ let randomize = () => {
 }
 
 let input = document.getElementById("input")
+
+input.addEventListener("keypress", function(event) {
+    if (event.key === "Enter") {
+        event.preventDefault();
+        document.querySelector('.input-btn').click();
+    }
+}); 
 let addplayer = () => {
     let val = input.value
     if(val.length > 0){
